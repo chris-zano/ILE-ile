dc67ee13b6472e6ed2943c3b0136ff04bffbfd21
Christian Solomon <cncs.chris@gmail.com>
2024-03-29 14:38:47 +0000

    update imports path

--- controllers/admin/forge/import.forge.js
+++ controllers/admin/forge/import.forge.js
@@ -1,6 +1,18 @@
-const lecturersProperties = ['lecturerId','firstName','lastName','faculty','assignedCourses'];
-const studentsproperties = ['studentId','firstName','lastName','program','year','level','faculty'];
+const lecturersProperties = ['lecturerId', 'firstName', 'lastName', 'faculty', 'assignedCourses'];
+const studentsproperties = ['studentId', 'firstName', 'lastName', 'program', 'year', 'level', 'faculty'];
 
-const validateImportedJSON = (jsonObject={}, properties=[]) => {
+const validateImportedJSON = (jsonObject = {}, properties = []) => {
+    if (!Array.isArray(properties)) {
+        throw new Error("Properties must be an array!");
+    }
 
+    const missingProperties = properties.filter((prop) => jsonObject.hasOwnProperty(prop));
+
+    return (missingProperties.length === 0);
+}
+
+const handleUserImport = (req, res) => {
+    const { userType, id, adminId, role } = req.query;
+    const { originalname, filename } = req.file;
+    const filePath = path.join(__dirname, '..', '..', '..', 'models', 'imports', filename);
 }
