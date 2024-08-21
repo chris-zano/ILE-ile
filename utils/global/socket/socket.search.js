36d568023632c9ecf21781d4045e9448fc401c41
Christian Solomon <cncs.chris@gmail.com>
2024-08-21 15:34:59 +0000

    add changes for subs and search@

--- utils/global/socket/socket.search.js
+++ utils/global/socket/socket.search.js
@@ -1,6 +1,11 @@
 import { findCourse, findStudent, findTutor } from "../../../controllers/admin/admin.search.js";
 
 const handleSearch = (category, searchInput) => {
+    const searchFunctions = {
+        'courses': findCourse,
+        'students': findStudent,
+        'lecturers': findTutor,
+    }
     switch (category) {
         case "courses":
             findCourse(socket,searchInput);
