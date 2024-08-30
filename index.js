53f37d0ac8bcd9568afa284b96d46cafe88336e2
Christian Solomon <cncs.chris@gmail.com>
2024-08-30 13:50:28 +0000

    update db host

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ else {
   //Connect to Database and start server
   (async () => {
     try {
-      await mongoose.connect(local_uri);
+      await mongoose.connect(uri);
       console.log("Connected to database");  
 
       callAndExecuteRequireStack(app);
