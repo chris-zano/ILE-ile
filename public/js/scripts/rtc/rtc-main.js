e9826549a378e51833bf24d4c7cd2260123109f3
Christian Solomon <cncs.chris@gmail.com>
2024-09-03 17:30:05 +0000

    refactor rtc-main

--- public/js/scripts/rtc/rtc-main.js
+++ public/js/scripts/rtc/rtc-main.js
@@ -1,141 +1,109 @@
 const socket = io();
 const videoGrid = document.getElementById('video-grid');
 const myGrid = document.getElementById('my-grid');
-const participantGrid = document.getElementById('participants-grid');
-var myPeer = new Peer();
-let count = 0;
-let myVideoStream; //the video stram is stored in this variable
-
-let lecturerStreamSet = false
-
-let currentPeer = null;
 const peers = {};
 const names = {};
 const participants = [];
 
+let myPeer = null;
+let count = 0;
+let myVideoStream;
+let currentPeer = null;
 let userName = undefined;
 let uid = undefined;
 let permissionClass = undefined;
 let studentId = undefined;
 let profilePicUrl = '';
 
-let userData = sessionStorage.getItem('auth-user');
-
-if (!userData) {
-    console.log("No data fetched")
-    alert("session is invalid");
-    window.location.replace('/login');
-}
-else {
-    userData = JSON.parse(userData);
-    const userType = userData.user;
-    const data = userData.data;
-    if (userType === "lecturer") {
-        userName = `${data.firstname} ${data.lastname}`;
-        uid = `${data.id}`;
-        permissionClass = "lecturer";
-        studentId = undefined;
-    }
-    else if (userType === "student") {
-        userName = `${data.firstName} ${data.lastName}-${data.studentId}_${data.classId}`;
-        uid = `${data.id}`;
-        permissionClass = "student";
-        studentId = data.studentId;
-    }
-    else {
-        alert("session is invalid");
-        window.location.replace('/login');
-    }
-
-    profilePicUrl = data.profilePicUrl;
-
-    // participants.push[{ userName, uid, permissionClass, studentId }];
-}
-
-const myVideo = document.createElement('video'); //div which contains the video
-myVideo.id = `user-video_${uid}`
-myVideo.muted = true;
-
-function formatAMPM(date) {
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
-    return strTime;
-}
-document.getElementById('time').innerText = formatAMPM(new Date) + " | Meeting";
-setInterval(setTime, 1000);
-function setTime() {
-    document.getElementById('time').innerText = formatAMPM(new Date) + " | Meeting";
-}
 
 
+const authenticateUserSession = () => {
+    try {
+        let userData = sessionStorage.getItem('auth-user');
 
-navigator.mediaDevices.getUserMedia({
-    video: true, //we want video
-    audio: true //we want audio
-}).then(async (stream) => {
-    // console.log("uid to be set: ", uid);
-    const tempuid = uid
-    myVideoStream = stream; //storing the video stream returned to the myVideoStream variable
-
-    console.log('adding my video stream', userName, permissionClass)
-    addVideoStream(myVideo, stream, userName, tempuid, true, 'me-loaded'); //appended my stream to 'video-grid' div
-    //add participant to list of participants
-    await addParticipant(ROOM_ID, { userName, uid, permissionClass, studentId, profilePicUrl });
-
-    myPeer.on('call', call => {
-        call.answer(stream);
-        const { name, userId, cuiid } = call.metadata
-        console.log("caller: ", { name, userId, cuiid })
-
-        const video = document.createElement('video');
-        let html = '<i class="fas fa-microphone"></i>'
-        video.innerHTML = html;
-        call.on('stream', userVideoStream => {
-            console.log('on stream video displayed', { name, cuiid });
-            addVideoStream(video, userVideoStream, name, cuiid, false, 'call-answer')
-        })
-        currentPeer = call;
-    })
-    socket.on('user-connected', ({ userId, name, cuid }) => {
-        console.log(`new user connected: `, { userId, name, cuid })
-        setTimeout(connectToNewUser, 1000, userId, name, cuid, stream);
-    });
-
-}).catch((err) => {
-    console.error('Error accessing media devices:', err);
-    alert('Error accessing media devices. Please check your permissions.');
-});
-
-socket.on("call-terminated", () => {
-    const coursePageUrl = constructCourseViewUrl(userData, ROOM_ID);
-    alert("Host has ended this call");
-    return window.location.replace(coursePageUrl);
-})
-
-socket.on('user-disconnected', userId => {
-    if (peers[userId]) {
-        peers[userId].close();
-        const videoElement = document.getElementById(`video-${userId}`);
-        // console.log("disconnected user's video is: ", videoElement)
-        if (videoElement) {
-            videoElement.remove();
+        if (!userData) {
+            console.log("No data fetched")
+            alert("session is invalid");
+            window.location.replace('/login');
         }
-        delete peers[userId];
+        else {
+            userData = JSON.parse(userData);
+            const userType = userData.user;
+            const data = userData.data;
+            if (userType === "lecturer") {
+                userName = `${data.firstname} ${data.lastname}`;
+                uid = `${data.id}`;
+                permissionClass = "lecturer";
+                studentId = undefined;
+            }
+            else if (userType === "student") {
+                userName = `${data.firstName} ${data.lastName}-${data.studentId}_${data.classId}`;
+                uid = `${data.id}`;
+                permissionClass = "student";
+                studentId = data.studentId;
+            }
+            else {
+                alert("session is invalid");
+                window.location.replace('/login');
+            }
+
+            profilePicUrl = data.profilePicUrl;
+        }
     }
-    cleanUpUI();
-});
+    catch (error) {
+        console.log('an error occured while authenticating the user');
+        console.error(error);
+    }
+}
 
-myPeer.on('open', id => {
-    socket.emit('join-room', ROOM_ID, id, userName, uid, permissionClass);
-});
+const configureNewPeer = () => {
+    try {
+        myPeer = new Peer({
+            config: {
+                'iceServers': [
+                    {
+                        urls: ["stun:eu-turn4.xirsys.com"]
+                    },
+                    {
+                        username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
+                        credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
+                        urls: [
+                            "turn:eu-turn4.xirsys.com:80?transport=udp",
+                            "turn:eu-turn4.xirsys.com:3478?transport=tcp"
+                        ]
+                    }
+                ]
+            }
+        });
+    }
+    catch (error) {
+        console.log('failed to configure new peer');
+        console.error(error);
+    }
+}
 
+const setLocalDateTime = () => {
+    function formatAMPM(date) {
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        let strTime = hours + ':' + minutes + ' ' + ampm;
+        return strTime;
+    }
 
-function connectToNewUser(userId, name, cuid, stream) {
+    function setTime() {
+        document.getElementById('time').innerText = formatAMPM(new Date) + " | Meeting";
+    }
+
+    setTime();
+
+    setInterval(setTime, 1000);
+}
+
+const connectToNewUser = (userId, name, cuid, stream) => {
     const call = myPeer.call(userId, stream, { metadata: { name: userName, userId: myPeer.id, cuiid: cuid } });
     const video = document.createElement('video');
 
@@ -160,7 +128,7 @@ function connectToNewUser(userId, name, cuid, stream) {
     currentPeer = call;
 }
 
-function addVideoStream(video, stream, name, cuid, state, caller) {
+const addVideoStream = (video, stream, name, cuid, state, caller) => {
     console.log("Adding a new video for: ",
         { video, name, cuid }
     )
@@ -207,6 +175,97 @@ function addVideoStream(video, stream, name, cuid, state, caller) {
         cleanUpUI();
     }, 20000);
 }
+
+const handlePeerCalls = (stream) => {
+    try {
+        myPeer.on('call', call => {
+            call.answer(stream);
+            const { name, userId, cuiid } = call.metadata
+
+            const video = document.createElement('video');
+            call.on('stream', userVideoStream => {
+                addVideoStream(video, userVideoStream, name, cuiid, false, 'call-answer')
+            })
+            currentPeer = call;
+        });
+
+        myPeer.on('open', id => {
+            socket.emit('join-room', ROOM_ID, id, userName, uid, permissionClass);
+        });
+
+    }
+    catch (error) {
+        console.log('Error occured while processing call');
+        console.error(error);
+    }
+}
+
+const rtcMainLib = async () => {
+
+    //authenticate user.
+    authenticateUserSession();
+
+    //configure  peerConnection
+    configureNewPeer();
+
+    //set the local date and time
+    setLocalDateTime();
+
+    //create user video element and set to mute
+    const myVideo = document.createElement('video'); //div which contains the video
+    myVideo.id = `user-video_${uid}`
+    myVideo.muted = true;
+
+
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+        myVideoStream = stream;
+
+        addVideoStream(myVideo, stream, userName, tempuid, true, 'me-loaded');
+        await addParticipant(ROOM_ID, { userName, uid, permissionClass, studentId, profilePicUrl });
+
+
+        handlePeerCalls(stream);
+
+        socket.on('user-connected', ({ userId, name, cuid }) => {
+            console.log(`new user connected: `, { userId, name, cuid })
+            setTimeout(connectToNewUser, 1000, userId, name, cuid, stream);
+        });
+
+        socket.on("call-terminated", () => {
+            const coursePageUrl = constructCourseViewUrl(userData, ROOM_ID);
+            alert("Host has ended this call");
+            return window.location.replace(coursePageUrl);
+        });
+
+        socket.on('user-disconnected', userId => {
+            if (peers[userId]) {
+                peers[userId].close();
+                const videoElement = document.getElementById(`video-${userId}`);
+                if (videoElement) {
+                    videoElement.remove();
+                }
+                delete peers[userId];
+            }
+            cleanUpUI();
+        });
+
+    } catch (error) {
+        console.error('Error accessing media devices:', error);
+        alert('Error accessing media devices. Please check your permissions.');
+    }
+}
+
+
+
+document.addEventListener("DOMContentLoaded", rtcMainLib);
+
+
+
+
+
+
+
 
 function cleanUpUI() {
     const intervalId = setInterval(() => {
